f6204477359f944d41d5d52484d5fbf54e4190d3
Johannes Mueller <github@johannes-mueller.org>
2020-06-01 21:03:16 +0200

    Step forward in lv2 implementation

--- sonarigo-lv2/src/lib.rs
+++ sonarigo-lv2/src/lib.rs
@@ -2,9 +2,48 @@
 extern crate lv2;
 
 use lv2::prelude::*;
+use lv2::lv2_atom as atom;
 
 use soundfonts::engine::EngineTrait;
 use soundfonts::sfz::engine;
+
+mod patch;
+
+#[uri("http://lv2plug.in/ns/ext/atom#Path")]
+struct AtomPath;
+
+impl<'a, 'b> Atom<'a, 'b> for AtomPath
+where
+    'a: 'b,
+{
+    type ReadParameter = ();
+    type ReadHandle = &'a str;
+
+    type WriteParameter = ();
+    type WriteHandle = AtomPathWriter<'a, 'b>;
+
+    fn read(body: Space<'a>, _: ()) -> Option<&'a str> {
+	body.data()
+            .and_then(|data| std::str::from_utf8(data).ok())
+            .map(|string| &string[..string.len() - 1])
+    }
+
+    fn init(frame: FramedMutSpace<'a, 'b>, _: ()) -> Option<AtomPathWriter<'a, 'b>> {
+        Some(AtomPathWriter { frame })
+    }
+}
+
+struct AtomPathWriter<'a, 'b> {
+    frame: FramedMutSpace<'a, 'b>
+}
+
+
+#[uri("http://lv2plug.in/ns/ext/state#StateChanged")]
+struct StateChanged;
+
+#[uri("http://johannes-mueller.org/oss/lv2/sonarigo#sfzfile")]
+struct SampleFile;
+
 
 #[derive(PortCollection)]
 pub struct Ports {
@@ -24,7 +63,14 @@ pub struct URIDs {
     atom: AtomURIDCollection,
     midi: MidiURIDCollection,
     unit: UnitURIDCollection,
+    patch: patch::PatchURIDCollection,
+    state_changed: URID<StateChanged>,
+    atom_path: URID<AtomPath>,
+
+    sfzfile: URID<SampleFile>,
 }
+
+
 
 #[uri("http://johannes-mueller.org/oss/lv2/sonarigo#lv2")]
 pub struct SonarigoLV2 {
@@ -57,20 +103,34 @@ impl Plugin for SonarigoLV2 {
             .unwrap();
 
 	for (timestamp, message) in control_sequence {
-            let timestamp: usize = match timestamp.as_frames() {
-                Some(ts) => ts as usize,
-                None => continue
+            match timestamp.as_frames() {
+		Some(ts) if ts > 0  => {
+		    let frame = ts as usize;
+		    self.engine.process(&mut ports.out_left[offset..frame], &mut ports.out_right[offset..frame]);
+		    offset = frame;
+		}
+		_ => {}
             };
 
-	    let message = match message.read(self.urids.midi.wmidi, ()) {
-		Some(msg) => msg,
-		None => continue
+	    if let Some(msg) = message.read(self.urids.midi.wmidi, ()) {
+		self.engine.midi_event(&msg);
 	    };
 
-	    self.engine.midi_event(&message);
+	    if let Some((header, mut object_reader)) = message.read(self.urids.atom.object, ()) {
+		if header.otype != self.urids.patch.set {
+		    continue;
+		}
+
+		if let Some(path) = self.parse_sfzfile_path(&mut object_reader) {
+		    println!("received path {}", path);
+		}
+	    }
 	}
 
-	self.engine.process(&mut ports.out_left, &mut ports.out_right);
+	let nsamples = ports.out_left.len();
+	if offset < nsamples {
+	    self.engine.process(&mut ports.out_left[offset..nsamples], &mut ports.out_right[offset..nsamples]);
+	}
 
 	let gain = match *ports.gain {
 	    g if g < -80.0 => 0.0,
@@ -80,7 +140,33 @@ impl Plugin for SonarigoLV2 {
 	for (l, r) in Iterator::zip(ports.out_left.iter_mut(), ports.out_right.iter_mut()) {
 	    *l *= gain;
 	    *r *= gain;
+
 	}
+    }
+}
+
+impl SonarigoLV2 {
+    fn parse_sfzfile_path<'a>(&self, object_reader: &mut atom::object::ObjectReader<'a>) -> Option<&'a str> {
+	if let Some((property_header, atom)) = object_reader.next() {
+	    if property_header.key != self.urids.patch.property {
+		return None;
+	    }
+	    if atom.read(self.urids.atom.urid, ()).map_or(true, |urid| urid != self.urids.sfzfile) {
+		return None;
+	    }
+	    if let Some((property_header, atom)) = object_reader.next() {
+		if property_header.key != self.urids.patch.value {
+		    return None;
+		}
+		let path = if let Some(path) = atom.read(self.urids.atom_path, ()) {
+		    path
+		} else {
+		    return None;
+		};
+		return Some(path);
+	    }
+	}
+	None
     }
 }
 
